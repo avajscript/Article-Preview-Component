3f9954a2d91d939a05c552f8f21dc1353be4156e
Matthew Pierce <lesbianhaircut@outlook.com>
2021-08-08 18:46:13 -0400

    c

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
 window.onload = function () {
   const el = document.querySelectorAll(".fas");
-  console.log(el);
+
   const preShare = document.getElementById("pre-share");
   const postShare = document.getElementById("post-share");
-  let container = document.querySelector(".container");
+  const container = document.querySelector(".container");
+  const desktopShare = document.querySelector(".desktop-share");
+
+  console.log(getComputedStyle(container).display);
 
   function toggleShare() {
-    postShare.style.display == "none"
-      ? (postShare.style.display = "flex")
-      : (postShare.style.display = "none");
+    if (getComputedStyle(container).display == "flex") {
+      desktopShare.style.display == "none"
+        ? (desktopShare.style.display = "flex")
+        : (desktopShare.style.display = "none");
+    } else {
+      postShare.style.display == "none"
+        ? (postShare.style.display = "flex")
+        : (postShare.style.display = "none");
 
-    preShare.style.display == "none"
-      ? (preShare.style.display = "block")
-      : (preShare.style.display = "none");
+      preShare.style.display == "none"
+        ? (preShare.style.display = "block")
+        : (preShare.style.display = "none");
+    }
   }
 
   el.forEach((button) => {
